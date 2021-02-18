7a005b1a1ff5dcd92e95b95ba466a5555a396e82
DS <971021hr@naver.com>
2021-02-18 12:17:54 +0900

    21.02.18 02

--- app/src/main/java/com/example/tantan/MainActivity.java
+++ app/src/main/java/com/example/tantan/MainActivity.java
@@ -15,6 +15,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    //21.02.17 sm,hk
+
     // FrameLayout에 각 메뉴의 Fragment를 바꿔 줌
     private FragmentManager fragmentManager = getSupportFragmentManager();
     // 4개의 메뉴에 들어갈 Fragment들
