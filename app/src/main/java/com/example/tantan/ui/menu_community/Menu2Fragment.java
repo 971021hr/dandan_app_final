29eed8374d686071d3bff97e1dce420bcd6e7ad8
DS <971021hr@naver.com>
2021-02-22 14:09:41 +0900

    21.02.22 hr 커뮤니티 버튼 디테일 페이징

--- app/src/main/java/com/example/tantan/ui/menu_community/Menu2Fragment.java
+++ app/src/main/java/com/example/tantan/ui/menu_community/Menu2Fragment.java
@@ -11,27 +11,30 @@ import android.widget.LinearLayout;
 import com.example.tantan.R;
 
 public class Menu2Fragment extends Fragment {
-/*
 
-    private View community_main;
-    private View community_detail;
-    private Button btn_detail;
+    Button btn_detail;
+    LinearLayout community_layout_main;
+    LinearLayout community_layout_detail;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_menu2, container, false);
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_menu2,container,false);
+
+        btn_detail = (Button) rootView.findViewById(R.id.btn_community_detail);
+        community_layout_main = (LinearLayout) rootView.findViewById(R.id.menu_community_main);
+        community_layout_detail = (LinearLayout) rootView.findViewById(R.id.menu_community_detail);
+
+        btn_detail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                community_layout_main.setVisibility(View.INVISIBLE);
+                community_layout_detail.setVisibility(View.VISIBLE);
+            }
+        });
+
+        return rootView;
     }
 
-    public void InitializeView(){
-        community_main = (LinearLayout)findViewById(R.id.menu_community_main);
-    }
-
-    public void onClick(View view){
-
+    public void onBackPressed(){
 
     }
-*/
-
-
-
-
 }
