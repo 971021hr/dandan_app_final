44f812d6f43fca77473f22c582851c19f0b176e5
semibang <obeyme39@naver.com>
2021-03-02 12:39:27 +0900

    0302 개강, 디자인 수정

--- app/src/main/java/com/example/tantan/ui/menu_add/menu_add.java
+++ app/src/main/java/com/example/tantan/ui/menu_add/menu_add.java
@@ -2,6 +2,7 @@ package com.example.tantan.ui.menu_add;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
@@ -10,12 +11,16 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.example.tantan.BottomNavigationHelper;
 import com.example.tantan.MainActivity;
 import com.example.tantan.R;
 
-public class menu_add extends Fragment {
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_menu3_1, container, false);
+public class menu_add  extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_menu3_1);
 
     }
 }
+
